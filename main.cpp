65606b4383ef3ec5d8fb55fc99b478ba5a774a07
rockystone77 <88238335+rockystone77@users.noreply.github.com>
2021-08-31 17:18:05 +0900

    created main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/* main.cpp tests the classes in our project and for practice.
+ * Peter Jung
+ * Date: 7-25-2021 (lol)
+ * Begun by: Joel Adams, for CS 112 at Calvin University.
+ */
+
+#include "SongTester.h"
+#include "PlayListTester.h"
+
+int main() {
+	SongTester sTester;
+	sTester.runTests();
+	PlayListTester plTester;
+	plTester.runTests();
+}
